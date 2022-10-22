8e7edc77b69d80e95e060b672ff7b53e2087dd35
Shlok Mohanty <59838234+ShlokMohanty@users.noreply.github.com>
2022-10-22 19:59:02 +0530

    Create __init__.pyi

---
+++ main/__init__.pyi
@@ -0,0 +1,13 @@
+from .convert import(
+  frames_to_samples as frames_to_samples,
+  frames_to_time as frames_to_time,
+  samples_to_frames as samples_to_frames,
+  samples_to_time as samples_to_time,
+  time_to_samples as time_to_samples,
+  time_to_frames as time_to_frames,
+  blocks_to_samples as blocks_to_samples,
+  blocks_to_frames as blocks_to_frames,
+  blocks_to_time as blocks_to_time,
+  blocks_to_hz as blocks_to_hz,
+  blocks_to_midi as 
+)
